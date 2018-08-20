6ccd0b77b900f7e7beba31b8830ffe43888e7408
ZSI2017 <13407121461@163.com>
2018-08-21 00:15:31 +0800

    transtionTo 动态组件渲染

--- src/install.js
+++ src/install.js
@@ -24,6 +24,10 @@ export function install (Vue) {
         this._routerRoot = this
         this._router = this.$options.router
         this._router.init(this)
+        // 将当前route 对象上的 _router 转换为响应式属性，
+        // <route-view>组件 访问 parent.$route  也就是访问 this._routerRoot._router
+        // 当 transtionTo 执行完成， 执行 updateRoute() 里面的 this.cb($route); 改变vue 实例上的_router 属性。
+        // 触发 set 构造器函数，从而触发 <route-view> 组件重新渲染组件。实现了页面跳转。
         Vue.util.defineReactive(this, '_route', this._router.history.current)
       } else {
         this._routerRoot = (this.$parent && this.$parent._routerRoot) || this
