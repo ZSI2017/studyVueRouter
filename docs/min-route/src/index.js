ff39207b9db3871b194790d354679b84eded01da
ZSI2017 <13407121461@163.com>
2018-09-19 00:32:42 +0800

    min-route

---
+++ docs/min-route/src/index.js
@@ -0,0 +1,42 @@
+/**
+ *
+ *  1. 事件监听
+ *  2. 钩子函数
+ *  3. 路由变化，页面渲染
+ *  4. 记录路由信息
+ *  5. 传递路由参数
+ *  6. 路由正则匹配
+ *  7. 路由配置信息管理
+ *  8. 路由参数解析
+ *  9.
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ */
+
+function init(options) {}
+
+function getHash() {
+  var href = window.location.href,
+      pos = href.indexOf("#");
+
+  if (index > -1) {
+    return href.slice(pos+1)
+  } else {
+    return ''
+  }
+}
+
+function setupListener() {
+  window.addEventListener('popstate', function() {
+    transitionTo(getHash())
+  },false)
+
+}
+
+function transitionTo(hash, cb) {
+
+}
+
+function resolvePath() {
+
+}
