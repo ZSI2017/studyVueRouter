90f6ea24639b18503e9b4b11ad606d97f5927215
ZSI2017 <13407121461@163.com>
2018-09-24 18:50:08 +0800

    finish sample route

--- docs/min-route/src/app-simple.js
+++ docs/min-route/src/app-simple.js
@@ -10,6 +10,9 @@ function init(options) {
         afterEachHooks = [],
         errorCds = [],
         currentRoute = {},
+        current = createRoute({
+           path: '/'
+         }, null),
         queueInit = [
           beforeEachHooks,
           renderRoute,
@@ -19,17 +22,18 @@ function init(options) {
   if(!~window.location.href.indexOf('#')){
     window.history.replaceState({}, "index", "#/")
   } else {
-    transitionTo()
+    transitionTo(getHash())
   }
 
 
   window.addEventListener(supportPushState ? 'popstate' : 'hashchange', function(e) {
-      transitionTo(e);
-    })
+      transitionTo(getHash());
+  })
 
   // 路由匹配渲染模块
   function renderRoute(current) {
-    render.innerHTML = current.component ? current.component.template : ''
+    console.log(current)
+    render.innerHTML = current.template ? current.template : ''
   }
 
   /**
@@ -37,51 +41,15 @@ function init(options) {
    * 利用regex 匹配path,并获取到path上的params
    * @return {[type]} [description]
    */
-  function matchRoute(hash) {
-    var currentHash = hash,        // 当前页面中的hash值，
-        pathMap = resolvePath(),    // path映射
-        location = {
-          hash: hash,
-          params: {},
-        }
 
-    for( var path in pathMap) {
-      if (pathMap.hasOwnProperty(path)) {
-        var record = pathMap[path],
-            m = hash.match(record.regex);
-        if (m) {
-          for (var i = 1, len = m.length; i < len; i++) {
-
-            var key = record.regex.keys[i-1]
-
-            if(key) {
-              location.params[key.name] = m[i];
-            }
-
-          }
-          location.record = record;
-          return location;
-        }
-      }
-    }
-
-    return location
-  }
-
-  function transitionTo(e, onAbort, onComplete) {
-    var location = matchRoute(getHash()),
-        current = {
-          hash: location.hash,
-          params: location.params,
-          component: location.record && location.record.component,
-          path: location.record && location.record.path
-        }
+  function transitionTo(location, onAbort, onComplete) {
+    var route = match(location),
 
         queue = flatten(queueInit),
         len = queue.length,
         iterator = function(fn) {
           try {
-            fn(current)
+            fn(route)
           } catch(e) {
             onAbort && onAbort(e);
 
@@ -94,12 +62,74 @@ function init(options) {
     for(var i = 0; i <= len; i++) {
       if (i == len) {
         onComplete && onComplete();
+        current = route;
 
         return;
       }
       iterator(queue[i])
     }
   }
+
+/**
+ *
+ * @param  {[type]} rawLocation [description]
+ * @return {[type]}             [description]
+ */
+function match(rawLocation) {
+  var location = normalizeLocation(rawLocation);
+
+  return createRoute(location, null);
+
+}
+
+function normalizeLocation(raw) {
+  var location = typeof raw === 'string' ? {path: raw} : raw,
+      currentHash = raw,
+      hash = location.path,       // 当前页面中的hash值，
+      pathMap = resolvePath();    // path映射
+
+  for( var path in pathMap) {
+    if (pathMap.hasOwnProperty(path)) {
+      var record = pathMap[path],
+          m = hash.match(record.regex);
+      if (m) {
+        for (var i = 1, len = m.length; i < len; i++) {
+
+          var key = record.regex.keys[i-1]
+
+          if(key) {
+            if(!location.params) {
+              location.params = {};
+            }
+
+            location.params[key.name] = m[i];
+          }
+        }
+
+        location = Object.assign(location, record);
+        return location;
+      }
+    }
+  }
+
+  return location
+}
+
+function createRoute(location, record) {
+  var query = location.query || {},
+      route = {
+        name: location.name || (record && record.name),
+        template: location.template || ( record && record.template),
+        record: record,
+        meta: record && record.meta || {},
+        path: location.path || '/',
+        hash: location.hash || '',
+        query: location.query,
+        params: location.params || {}
+      }
+
+  return Object.freeze(route)
+}
 
 /**
  * 暂时针对没有父子嵌套的路由，
@@ -110,7 +140,7 @@ function init(options) {
         pathMap = {};
 
     while(len--) {
-      addRoute( routes[len], pathMap)
+      addRoute(routes[len], pathMap)
     }
 
     return pathMap;
@@ -120,7 +150,7 @@ function init(options) {
     var href = window.location.href,
         pos = href.indexOf('#');
 
-    if(pos == -1) {
+    if (pos == -1) {
       return ''
     }
 
@@ -175,9 +205,28 @@ function init(options) {
    */
   function go(n) {
     window.history.go(n)
+  }
+
+  function push(location) {
+    transitionTo(location, null, function() {
+      window.history.pushState(null,null,getUrl(location.path))
+    })
 
   }
 
+  function  getUrl(path) {
+    var href = window.location.href,
+        hash = href.indexOf('#'),
+        url;
+
+    if (hash > -1) {
+      url = href.slice(0, hash)
+    } else {
+      url = href;
+    }
+
+    return url + "#" + path;
+  }
   /**
    * 路由后退
    * @return {[type]} [description]
@@ -205,6 +254,7 @@ function init(options) {
     go: go,
     back: back,
     forward: forward,
+    push: push,
     onError: onError,
     addRoute: addRoute,
     beforeEach: beforeEach,
--- docs/min-route/src/utils.js
+++ docs/min-route/src/utils.js
@@ -24,6 +24,39 @@ function flatten(queue) {
 }
 
 /**
+ * 获取hash 值
+ * @return {[type]} [description]
+ */
+function getHash() {
+  var href = window.location.href,
+      pos = href.indexOf('#');
+
+  if (pos == -1) {
+    return ''
+  }
+
+  return decodeURI(href.slice(pos+1));
+}
+
+/**
+ * 替换完整的url
+ * @param  {[type]} path [description]
+ * @return {[type]}      [description]
+ */
+function getUrl(path) {
+  var href = window.location.href,
+      hash = href.indexOf('#'),
+      url;
+
+  if (hash > -1) {
+    url = href.slice(0, hash)
+  } else {
+    url = href;
+  }
+
+  return url + "#" + path;
+}
+/**
  *  顺序执行函数的迭代器
  */
 function iterator(queue, hooks, onComplete) {
